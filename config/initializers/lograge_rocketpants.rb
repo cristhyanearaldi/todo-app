84f2ca6ad4bf908c245ff0c3dd66e319740671cc
cristhyane araldi <cristhyane.araldi@hotmail.com>
2023-10-20 09:20:43 -0300

    add and configure gem lograge

---
+++ config/initializers/lograge_rocketpants.rb
@@ -0,0 +1,12 @@
+# Come from here:
+#   https://github.com/Sutto/rocket_pants/issues/111
+app = Rails.application
+if app.config.lograge.enabled
+  ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
+    case subscriber
+      when ActionController::LogSubscriber
+        Lograge.unsubscribe(:rocket_pants, subscriber)
+    end
+  end
+  Lograge::RequestLogSubscriber.attach_to :rocket_pants
+end
